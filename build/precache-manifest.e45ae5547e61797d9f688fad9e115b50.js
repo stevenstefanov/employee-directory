177c10895319a06e54e13a52fec411bdf4bb67dd
Steven Stefanov <77702697+stevenstefanov@users.noreply.github.com>
2021-06-13 20:53:21 -0400

    deployed first React app to GitHub.

---
+++ build/precache-manifest.e45ae5547e61797d9f688fad9e115b50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b6f8026af5a78969f3530b153e52d79",
+    "url": "/employee-directory/index.html"
+  },
+  {
+    "revision": "c85635143026af4a75f7",
+    "url": "/employee-directory/static/css/2.9233ae65.chunk.css"
+  },
+  {
+    "revision": "5966f59833e427e7f2ed",
+    "url": "/employee-directory/static/css/main.07ebd963.chunk.css"
+  },
+  {
+    "revision": "c85635143026af4a75f7",
+    "url": "/employee-directory/static/js/2.0a6ff45d.chunk.js"
+  },
+  {
+    "revision": "5966f59833e427e7f2ed",
+    "url": "/employee-directory/static/js/main.5997e2c9.chunk.js"
+  },
+  {
+    "revision": "77a50e16a26474c82f8d",
+    "url": "/employee-directory/static/js/runtime~main.abed8607.js"
+  }
+]);
